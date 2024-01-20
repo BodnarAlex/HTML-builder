8c7eb00fe32bce7f1c430613c337439d4d2c702a
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-20 21:08:48 +0200

    fix: delete entrails of directory before filling at 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,22 +3,27 @@ const path = require('path');
 const correctPathFrom = path.resolve(__dirname, "files");
 const correctPathTo = path.resolve(__dirname, "files-copy");
 
-fs.mkdir(correctPathTo, { recursive: true }, (err) => {
+fs.rm(correctPathTo, { recursive: true, force: true }, (err) => {
         if (err) {
-            return console.error(err);
+            console.log(err);
         } else{
-            fs.readdir(correctPathFrom, (err, files) => {
+            fs.mkdir(correctPathTo, { recursive: true }, (err) => {
                 if (err) {
                     console.log(err);
-                } else {
-                    files.forEach(file => {
-                        fs.copyFile(path.resolve(correctPathFrom, file), path.resolve(correctPathTo, file), (err) => {
-                            if (err) {
-                                console.log(err);
-                            }
-                        });
+                } else{
+                    fs.readdir(correctPathFrom, (err, files) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            files.forEach(file => {
+                                fs.copyFile(path.resolve(correctPathFrom, file), path.resolve(correctPathTo, file), (err) => {
+                                    if (err)
+                                        console.log(err);
+                                });
+                            })
+                        }
                     })
                 }
-            })
+            });
         }
     });
