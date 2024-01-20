d217ca2325e266af6b679347d36f5cb62a482389
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-20 20:44:57 +0200

    feat: add script for copy files at 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const correctPathFrom = path.resolve(__dirname, "files");
+const correctPathTo = path.resolve(__dirname, "files-copy");
+
+fs.mkdir(correctPathTo, { recursive: true }, (err) => {
+        if (err) {
+            return console.error(err);
+        } else{
+            fs.readdir(correctPathFrom, (err, files) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    files.forEach(file => {
+                        fs.copyFile(path.resolve(correctPathFrom, file), path.resolve(correctPathTo, file), (err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                    })
+                }
+            })
+        }
+    });
