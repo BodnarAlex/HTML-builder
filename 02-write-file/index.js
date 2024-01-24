c56daaee5bd63c392fe23ad8e90f7c59e38fc9fc
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-01-24 20:31:27 +0200

    fix: return script for node 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,3 +6,19 @@ let rl = readline.createInterface(process.stdin, process.stdout);
 
 const fs = require("fs");
 const stream = fs.createWriteStream(correctPath, "utf-8");
+
+const greeting = "Hello. What do you wish to write?..\n";
+const farewell = "Quite. It is enough.";
+
+rl.setPrompt(greeting);
+rl.prompt();
+rl.on('line', (text) => {
+    if (text === 'exit')
+        rl.close();
+    else
+        stream.write(text + "\n");
+});
+
+rl.on('close', () => {
+    console.log(farewell);
+});
